203c57bc07ca51e7372cd1a29ab6abfdf6d05f0a
Felipe Barbosa <54113774+barbosafelipe@users.noreply.github.com>
2020-08-10 17:11:33 -0300

    Update uri1061.cpp

--- uri1061/uri1061/uri1061.cpp
+++ uri1061/uri1061/uri1061.cpp
@@ -21,7 +21,7 @@ int main()
 	int q1, q2, res;
 
 	q1 = segundo + minuto * 60 + hora * 60 * 60 + dia * 60 * 60 * 24;
-	q2 = segundo + minutof * 60 + horaf * 60 * 60 + diaf * 60 * 60 * 24;
+	q2 = segundof + minutof * 60 + horaf * 60 * 60 + diaf * 60 * 60 * 24;
 
 	res = q2 - q1;
 
